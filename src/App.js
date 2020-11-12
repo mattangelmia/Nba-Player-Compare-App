8abcae4572d6d50c9f78c73d6d6423f89227d812
rqsell <68514875+rqsell@users.noreply.github.com>
2020-11-12 17:15:17 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -7,8 +7,27 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Who's the better Player?</h1>
-      <Player></Player>
+    
+    <div className= 'header'>
+    <img src ="./logo.png" className="logo"></img>
+    </div>
+    <div className = "page">
+    <h1>See how they level up</h1>
+    </div>
+
+    <div className= "game">
+    <article>
+    <Player></Player>
+    </article>
+
+    <article>
+    <Player></Player>
+    </article>
+
+    </div>
+ 
+     
+      
     </div>
   );
 }
--- src/Player.js
+++ src/Player.js
@@ -2,20 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react";
 import axios from 'axios';
-
-
 function Player() {
     const [playerName, setPlayerName] = useState("");
     const [query, setQuery] = useState("")
+    const [playerInfo, setPlayerInfo] = useState([])
     const [playerStats, setPlayerStats] = useState({});
-
+    const [display, setDisplay] = useState(false)
     const handleSubmit = (e) => {
         e.preventDefault();
         getName()
         console.log(playerName)
-
+        console.log(playerStats)
     }
-
     // const handleChange = (e) => {
     //     const replace = e.target.value.split(" ").join("_")
     //     if (replace.length > 0) {
@@ -25,51 +23,72 @@ function Player() {
     //         alert("Please type players name")
     //     }
     // }
-
+ 
     const onType = (e) => {
         setQuery(e.target.value);
         setPlayerName(query);
-
     };
-
-
     const getStats = (playerId) => {
         const response = axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2019&player_ids[]=${playerId}`)
             .then(async (res) => {
                 console.log(res.data)
-                setPlayerStats(res.data.data[0])
+                setPlayerStats(res?.data?.data[0])
+                console.log(playerStats.pts)
             }).catch(err => {
                 console.log(err)
             })
     }
-    const getName = () => {
-        const result = axios.get(`https://www.balldontlie.io/api/v1/players?search=${playerName}`)
+    const getName = async (search) => {
+        const result = await axios.get(`https://www.balldontlie.io/api/v1/players?search=${playerName}`)
             .then(async (res) => {
                 console.log(res.data)
-
-                await getStats(res.data.data[0].id)
+                setPlayerInfo(res?.data?.data)
+                console.log(playerInfo)
+                if (res.data.data[0] === undefined) {
+                    alert("This Player is either injured or not playing")
+                }
+                else if (res.data.data.length > 1) {
+                    alert("Please specify the name more")
+                }
+                else {
+                    await getStats(res.data.data[0].id)
+                }
             })
             .catch(err => {
                 console.log(err)
             })
-
-
     }
     return (
-        <div className="App">
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <h1>Name </h1>
-                    <input type="text" onChange={onType} value={query} placeholder="Enter Player Name">
-                    </input>
-                </label>
-                <input type="submit" value="Submit"></input>
-            </form>
-
-            <button onClick={getName}>Names</button>
-            <button onClick={getStats}>Stats</button>
+        <div>
+            <div className="App">
+                <form className = "search" onSubmit={handleSubmit}>
+                    <label>
+                        <input className='input'type="text" onChange={onType} value={query} placeholder="Enter Player Name">
+                        </input>
+                    </label>
+                    <input type="submit" value="Submit" className='thing' ></input>
+                </form>
+    
+            </div>
+            <div>
+          
+                {playerInfo?.map((info) => (
+                    <ul className = 'statdisplay'>
+                    <img className= 'bluedude' src= './bluedude.png'></img>
+                    
+                        <h3> First Name: {info.first_name} </h3>
+                        <h3> Last Name: {info.last_name}</h3>
+                        <h3>Games Played: {playerStats.games_played}</h3>
+                <h3>Stats: {playerStats.pts}</h3>
+                    </ul>
+                ))}
+         
+            </div>
+            <div>
+                
+                
+            </div>
         </div>
     );
 }
-
 export default Player;
